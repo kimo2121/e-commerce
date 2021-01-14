dca6593d23abf1db69b9d24a2d91d6e99b929fa3
Muhammed khaled <muhammmed.k95@gmail.com>
2021-01-14 09:08:16 +0200

    try to pull from Kimo

--- src/components/HeaderItem/HeaderItem.js
+++ src/components/HeaderItem/HeaderItem.js
@@ -16,18 +16,13 @@ const defualtStyles = {
 const styles = {
   border: "1px solid black",
   borderRadius: "6px",
-<<<<<<< HEAD
   margin: " .5rem",
-=======
-  margin: "0 1rem",
->>>>>>> 8afad4c7bdae8f19a965980e003bde751923118a
   padding: ".3rem",
   background: "black",
   color: "grey ",
   position: "relative",
 };
 
-<<<<<<< HEAD
 export default function HeaderItem({ data, category, subcategory }) {
   const [activeLink, setactiveLink] = useState({
     index: 0,
@@ -37,22 +32,12 @@ export default function HeaderItem({ data, category, subcategory }) {
   // console.log({ category });
   return (
     <div style={{ display: "flex", flexFlow: "wrap" }}>
-=======
-export default function HeaderItem({ data, category }) {
-  const [activeLink, setactiveLink] = useState({ index: "", active: false });
-  return (
-    <div style={{ display: "flex" }}>
->>>>>>> 8afad4c7bdae8f19a965980e003bde751923118a
       {data.map((item, index) => (
         <div
           key={index}
           style={
-<<<<<<< HEAD
             (activeLink.index === index && activeLink.active) ||
             subcategory === item
-=======
-            activeLink.index === index && activeLink.active
->>>>>>> 8afad4c7bdae8f19a965980e003bde751923118a
               ? styles
               : defualtStyles
           }
@@ -61,12 +46,8 @@ export default function HeaderItem({ data, category }) {
             to={`/${category && category}/${item}`}
             style={{
               color:
-<<<<<<< HEAD
                 (activeLink.index === index && activeLink.active) ||
                 subcategory === item
-=======
-                activeLink.index === index && activeLink.active
->>>>>>> 8afad4c7bdae8f19a965980e003bde751923118a
                   ? "white"
                   : "black",
             }}
