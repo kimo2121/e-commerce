286b977fe93f2b387508151531153ff3bd62f137
kimo2121 <kareem.gaad@gmail.com>
2021-01-14 07:26:58 +0200

    dines

--- src/redux/orders/order.actions.js
+++
@@ -1,5 +0,0 @@
-// import OrderdItemsActionTypes from "./order.types";
-
-// export const completeOrder = () => ({
-//   type: OrderdItemsActionTypes.COMPLETE_ORDER,
-// });
--- src/redux/orders/order.types.js
+++
@@ -1,5 +0,0 @@
-// const OrderdItemsActionTypes = {
-//   COMPLETE_ORDER: "COMPLETE_ORDER",
-// };
-
-// export default OrderdItemsActionTypes;
