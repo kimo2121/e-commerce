c2008b09a890c83f2be1c7f2a76d8e7537c4134c
Muhammed khaled <muhammmed.k95@gmail.com>
2021-01-27 15:56:27 +0200

    nothing to say lol

--- src/components/Reviews/ReviewCard.js
+++ src/components/Reviews/ReviewCard.js
@@ -2,14 +2,7 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Rating from "@material-ui/lab/Rating";
 import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
-// import ProductImgList from "../../components/ProductImgList/ProductImgList";
 
-// const imgStyles = {
-//   width: "5rem",
-//   height: "5rem",
-//   objectFit: "contain",
-//   margin: ".5rem 0px",
-// };
 export default function ReviewCard({ review }) {
   let width = window.innerWidth;
 
@@ -32,9 +25,7 @@ export default function ReviewCard({ review }) {
           <p style={{ marginLeft: "1rem" }}> {review.productCondition[2]} </p>
         </div>
         <p> {review.body} </p>
-        {/* <ProductImgList style={imgStyles} /> */}
         <div style={{ display: "flex" }}>
-          {/* <p>2020-11-26 04:21:40</p> */}
           <p> {review.createdAt} </p>
           <ThumbUpAltOutlinedIcon
             style={{ margin: ".5rem .5rem .5rem 1.5rem", fontSize: "1rem" }}
